a0cdec338727b4ac87318c38d09e0db27db4eee8
darkMatterChimpanzee <dhrvmohapatra@gmail.com>
2022-05-25 09:00:31 +0530

    feat: Add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   const [account, setAccount] = useState(null)
@@ -38,20 +39,48 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
+    <div className='min-h-screen '>
+      <div className='sm:h-10'>
+        <nav className='flex mx-auto text-black/60 bg-slate-600'>
+          <Link href="/">
+            <a>
+              <img src="/logo.png" alt="crowdFund logo" className='h-20 object-contain my-5'/>
+            </a>
+          </Link>
 
-    <div>
+          <div className='rounded-md my-10 p-3 mx-4'>
+            {
+              !account && (
+                <div>
+                  <button className='rounded-md my-10 bg-green-200 p-3 mx-4' onClick={connect}>web3 Connect</button>
+                </div>
+              )
+            }
+            {
+              account && <p>{account.length > 10 ? account.substr(0, 9) + "..." : account}</p>
+            }
+          </div>
 
-      <div>
-        {
-          !account && (
-            <div>
-              <button onClick={connect}>Connect</button>
-            </div>
-          )
-        }
-        {
-          account && <p>{account}</p>
-        }
+
+          <div>
+            <Link href="/howItWorks" className='px-2'>
+              <button className='rounded-md my-10 bg-green-200 p-3 mx-4' >How it Works</button>
+            </Link>
+
+            <Link href="/createFR">
+              <button className='rounded-md my-10 bg-green-200 p-3 mx-4' >Create Fundraise</button>
+            </Link>
+
+            <Link href="/withdrawals">
+              <button className='rounded-md my-10 bg-green-200 p-3 mx-4' >Withdraw</button>
+            </Link>
+
+            <Link href="/requestRefund">
+              <button className='rounded-md my-10 bg-green-200 p-3 mx-4' >Refund</button>
+            </Link>
+          </div>
+        </nav>
+
       </div>
 
       <AccountContext.Provider value={account}>
